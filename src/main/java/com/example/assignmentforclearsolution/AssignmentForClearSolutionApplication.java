541da2b83fe6a1157db979a9e649beeec1ba12aa
Denis Kalyakin <denis.kalyakin999@gmail.com>
2024-04-24 21:21:22 +0300

    first commit

---
+++ src/main/java/com/example/assignmentforclearsolution/AssignmentForClearSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignmentforclearsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentForClearSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentForClearSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignmentforclearsolution/AssignmentForClearSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentforclearsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentForClearSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
